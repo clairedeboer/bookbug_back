bfa509c33b220de43ab1a66aac5ca55bfde0cd1e
clairedeboer <claire.deboer@gmail.com>
2021-02-25 16:14:23 -0500

    seeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
 class UsersController < ApplicationController
-  has_many :user_books
-  has_many :books, through: :user_books
-  has_many :reviews, through: :books
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :user_books
+  has_many :books, through: :user_books
+  has_many :reviews, through: :books
 end
